a6b116d4ed1f38942b028d90a62eef6516f41306
iamanquachs <106462008+iamanquachs@users.noreply.github.com>
2024-02-25 22:20:01 +0700

    Update axiosClient.js

--- src/pages/api/axiosClient.js
+++ src/pages/api/axiosClient.js
@@ -2,11 +2,10 @@ import axios from "axios";
 import { parse, stringify } from "qs";
 
 // const token =
-//   "eyJ0eXAi.iJKV1QiLCJhbGci.iJIUzI1NiJ9OeyJtc2R2IjoiMjIwMjIwMTA1NDA2MzciLCJtc2RuIjoiMDkwNzY3.DIzNCIsInRlbmR2IjoiTkhcdTAwYzAgVEhVXHUxZWQwQyBBTiBUXHUwMGMyTSIsImV4cGlyZWQi.jE3MDQzMzI2MzR9Okb16o93E9eAX28s.27VZsj94vZ9L2Ts6Z6PBh8g8Rz8";
+//   "eyJ0eXAi.iJKV1QiLCJhbGci.iJIUzI1NiJ9OeyJtc2R2IjoiMjIwMjIwMTA1NDA2MzciLCJtc2RuIjoiMDkwNzY3.DIzNCIsInRlbmR2IjoiTkhcdTAwYzAgVEhVXHUxZWQwQyBBTiBUXHUwMGM1yTSIsImV4cGlyZWQi.jE3MDQzMzI2MzR9Okb16o93E9eAX28s.27VZsj94vZ9L2Ts6Z6PBh8g8Rz8";
 
 const token = localStorage.getItem("token");
 const axiosClient = axios.create({
-  baseURL: "https://api.duoctaynam.vn",
   // baseURL: "https://localhost/TPS_nextjs/Backend/api",
   headers: { Authorization: `bearer ` + token },
   paramsSerializer: {
